1d970a33f088935067c609aedc52fb704db29114
darknessblade <darknessbladers@gmail.com>
2022-12-01 12:11:21 +0100

    Update config.h

--- void9/config.h
+++ void9/config.h
@@ -1,5 +1,28 @@
-// Copyright 2022 Victor Lucachi (@Victor Lucachi)
-// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
 
-#pragma once
+/* disable debug print */
+//#define NO_DEBUG
 
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+#define ENCODERS_PAD_A { GP0, GP1 }
+#define ENCODERS_PAD_B { GP2, GP3 }
+#define ENCODERS_PAD_C { GP4, GP5 }
+#define ENCODERS_PAD_D { GP6, GP7 }
+#define ENCODERS_PAD_E { GP8, GP9 }
+#define ENCODERS_PAD_F { GP10, GP11 }
+#define ENCODERS_PAD_G { GP12, GP13 }
+#define ENCODERS_PAD_H { GP14, GP15 }
+#define ENCODERS_PAD_I { GP16, GP17 }
